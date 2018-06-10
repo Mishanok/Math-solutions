90f8ae473985ab7adf3feef85306add9779433e4
Michael Mykytyuk <Mishadrug98@gmail.com>
2018-06-11 00:18:33 +0300

    Binary Search
    I have written an algorythm of search the element in Binary Tree

---
+++ Binary Search/ForTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public class Node
+{
+    public int Value { get; set; }
+
+    public Node Left { get; set; }
+
+    public Node Right { get; set; }
+
+    public Node(int value, Node left, Node right)
+    {
+        Value = value;
+        Left = left;
+        Right = right;
+    }
+}
+
+public class BinarySearchTree
+{
+    public static bool Contains(Node root, int value)
+    {
+        if(value > root.Value)
+        {
+            if (root.Right != null)
+            {
+                if (root.Right.Value == value) return true;
+                else return Contains(root.Right, value);
+            }
+            else return false;
+        }
+
+        if (value < root.Value)
+        {
+            if (root.Left != null)
+            {
+                if (root.Left.Value == value) return true;
+                else return Contains(root.Left, value);
+            }
+            else return false;
+        }
+        else return true;
+    }
+
+    public static void Main(string[] args)
+    {
+        Node n1 = new Node(1, null, null);
+        Node n3 = new Node(3, null, null);
+        Node n2 = new Node(2, n1, n3);
+
+        Console.WriteLine(Contains(n2, 3));
+        Console.ReadLine();
+    }
+}
